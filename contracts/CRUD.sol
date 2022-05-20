fa7db77c9c98c67b2e76b8db3599f78cbc0ebd9a
rajanlagah <rajanlagah@gmail.com>
2022-05-21 02:18:37 +0530

    ✨  Done app

--- contracts/CRUD.sol
+++ contracts/CRUD.sol
@@ -1,60 +1,60 @@
 pragma solidity ^0.5.10;
 
 contract CRUD {
- uint public maxId = 1; // because if we delete 0th element from array then getUserIndex will return index 0 as default for users[0].id will be set 0
- struct User {
-  uint id;
-  string name;
- }
+    uint256 public maxId = 1; // because if we delete 0th element from array then getUserIndex will return index 0 as default for users[0].id will be set 0
+    struct User {
+        uint256 id;
+        string name;
+    }
 
- User[] public users;
+    User[] public users;
 
     // get index for id
 
-    function getUserIndex(uint _id) view internal returns(uint index){
-        for( uint i = 0; i < users.length ; i++){
-            if(users[i].id == _id){
+    function getUserIndex(uint256 _id) internal view returns (uint256 index) {
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == _id) {
                 return i;
             }
         }
 
-        revert('User does not exist');
+        revert("User does not exist");
     }
 
-    // get user 
-    function getUser(uint _id) view public returns(uint, string memory){
-        uint userIndex = getUserIndex(_id);
-        return (users[userIndex].id,users[userIndex].name);
+    // get user
+    function getUser(uint256 _id) public view returns (uint256, string memory) {
+        uint256 userIndex = getUserIndex(_id);
+        return (users[userIndex].id, users[userIndex].name);
     }
-   // get all user 
+
+    // get all user
     // function getAll() view public returns(User[] memory){
     //     return users;
     // }
 
-    function sayHi() public returns(uint){
-
-        users.push(User(maxId,"RAJAN"));
+    function sayHi() public returns (uint256) {
+        users.push(User(maxId, "RAJAN"));
         maxId++;
         return users.length;
         // return users.length;
     }
 
     // add
-    function addUser(string memory _name) public returns(uint){
-        users.push(User(maxId,_name));
+    function addUser(string memory _name) public returns (uint256) {
+        users.push(User(maxId, _name));
         maxId++;
         return users.length;
     }
-    
+
     // update
-    function updateUser( uint _id, string memory _newName) public{
-        uint indexOfUser = getUserIndex(_id);
+    function updateUser(uint256 _id, string memory _newName) public {
+        uint256 indexOfUser = getUserIndex(_id);
         users[indexOfUser].name = _newName;
     }
 
     // delete
-    function deleteUser(uint _id) public {
-        uint userIndex = getUserIndex(_id);
+    function deleteUser(uint256 _id) public {
+        uint256 userIndex = getUserIndex(_id);
         delete users[userIndex];
     }
 }
--- web/react-app/src/App.js
+++ web/react-app/src/App.js
@@ -1,93 +1,149 @@
-import React from 'react'
-import Web3 from 'web3'
-import { getWeb3 } from './utils';
+import React from "react";
+import Web3 from "web3";
+import { getWeb3 } from "./utils";
 
-import logo from './logo.svg';
-import './App.css';
-import CRUD from './contracts/CRUD.json'
+import logo from "./logo.svg";
+import "./App.css";
+import CRUD from "./contracts/CRUD.json";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      _web3:null,
-      contract:null,
-      accounts:[],
-      userId:1,
-    }
+      _web3: null,
+      contract: null,
+      accounts: [],
+      userId: 1,
+      username: ""
+    };
   }
 
-  componentDidMount(){
-    try{
-      this.getSetWeb3()
-    }catch(e){
-      console.log("ERROR IN INIT",e)
+  componentDidMount() {
+    try {
+      this.getSetWeb3();
+    } catch (e) {
+      console.log("ERROR IN INIT", e);
     }
   }
 
   getSetWeb3 = async () => {
-    try{
-        const web3 = await getWeb3();
-        const accounts = await web3.eth.getAccounts();
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = CRUD.networks[networkId];
-        const contract = new web3.eth.Contract(
-          CRUD.abi,
-          deployedNetwork && deployedNetwork.address,
-        );
+    try {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = CRUD.networks[networkId];
+      const contract = new web3.eth.Contract(
+        CRUD.abi,
+        deployedNetwork && deployedNetwork.address
+      );
 
-        this.setState({ web3, accounts, contract }, this.addUser);
-
-    }catch(e){
-      console.log("ERROR IN INIT 0",e)
+      this.setState({ web3, accounts, contract }, this.addUser);
+    } catch (e) {
+      console.log("ERROR IN INIT 0", e);
     }
-  }
+  };
 
-  addUser = async () =>{
-    const { contract, accounts } = this.state
+  addUser = async () => {
+    const { contract, accounts, username } = this.state;
     // debugger
-    try{
-        contract.methods.sayHi()
-          .send({
-              from: accounts[0],
-              gas: 3000000,
-              gasPrice: Web3.utils.toWei('10', 'gwei')
-
-          })
-            .then( res => {
-              console.log("res",res)
-            })
-            .catch(e => console.log(e.message))
-    }catch(e){
-      console.log("ADD USER",e)
-
+    console.log("username", username);
+    try {
+      contract.methods
+        .addUser(username)
+        .send({
+          from: accounts[0],
+          gas: 3000000,
+          gasPrice: Web3.utils.toWei("10", "gwei")
+        })
+        .then((res) => {
+          console.log("res", res);
+        })
+        .catch((e) => console.log(e.message));
+    } catch (e) {
+      console.log("ADD USER", e);
     }
-  }
-  getUser = async () =>{
-    const { contract, accounts , userId } = this.state
-    try{
+  };
+
+  deleteUser = async () => {
+    const { contract, accounts, userId } = this.state;
+    try {
+      contract.methods
+        .deleteUser(userId)
+        .send({
+          from: accounts[0],
+          gas: 3000000,
+          gasPrice: Web3.utils.toWei("10", "gwei")
+        })
+        .then((res) => {
+          console.log("res", res);
+        })
+        .catch((e) => {
+          console.log("ERROR in delete", e);
+        });
+    } catch (e) {
+      console.log("ERROR in delete", e);
+    }
+  };
+
+  updateUser = async () => {
+    const { contract, accounts, username, userId } = this.state;
+
+    try {
+      contract.methods
+        .updateUser(userId, username)
+        .send({
+          from: accounts[0],
+          gas: 3000000,
+          gasPrice: Web3.utils.toWei("10", "gwei")
+        })
+        .then((res) => {
+          console.log("RES", res);
+        })
+        .catch((e) => console.log("ERROR IN UPDATEING USER", e));
+    } catch (e) {
+      console.log("ERROR IN UPDATEING USER", e);
+    }
+  };
+
+  getUser = async () => {
+    const { contract, accounts, userId } = this.state;
+    console.log(userId);
+    try {
       contract.methods
         .getUser(parseInt(userId))
         .call()
-        .then( res => {
-          console.log("res",res)
-          console.log("res",res[0])
+        .then((res) => {
+          console.log("res", res);
+          console.log("res", res[0]);
         })
-    .catch(e => console.log(e))
-    }catch(e){
-      console.log("GET USER",e)
+        .catch((e) => console.log(e));
+    } catch (e) {
+      console.log("GET USER", e);
     }
-  }
+  };
 
-  render(){
-    const { userId } = this.state
+  render() {
+    const { userId, username } = this.state;
     return (
       <div>
         CRUD
+        <input
+          value={username}
+          onChange={(e) => this.setState({ username: e.target.value })}
+          type="text"
+        />
         <button onClick={this.addUser}>Add user</button>
-        <input value={userId} onChange = {(e)=>this.setState({userId:e.target.value})} type='number'/>
-        <button onClick={this.getUser}>get user</button>
+        <br />
+        <br />
+        <input
+          value={userId}
+          onChange={(e) => this.setState({ userId: e.target.value })}
+          type="number"
+        />
+        <button onClick={this.getUser}>get user</button> <br/>
+        <button onClick={this.deleteUser}>delete user</button> <br/>
+        <button onClick={this.updateUser}>udpate user</button> <br/>
       </div>
     );
   }
